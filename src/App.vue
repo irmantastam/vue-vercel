070b8ccac69f3cf8419f42fdad02987cac7bdd18
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-07-12 15:33:51 +0300

    add basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img
+    class="logo"
+    alt="Irmantas Tamašauskas coffee break"
+    title="Irmantas Tamašauskas coffee break"
+    src="./assets/irmantas_tamasauskas.jpg"
+  />
+  <HelloWorld msg="Welcome Folks!" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
+:root {
+  --main-color: #323232;
+  --secondary-color: #222222;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--main-color);
   margin-top: 60px;
+}
+
+img.logo {
+  width: 350px;
+  height: 350px;
+  object-fit: cover;
+  border-radius: 50%;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,57 +2,87 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      My name is Irmantas and I'm a Frontend developer based in Kaunas,
+      Lithuania.<br />
+      I love ☕ and ⌨️
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Look me up here</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li>
+        <a
+          href="https://github.com/irmantastam"
+          target="_blank"
+          rel="noopener"
+          title="Github"
+          ><img
+            alt="Irmantas Tamašauskas Github"
+            title="Github"
+            src="../assets/github.svg"
+          />irmantastam</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.facebook.com/irmantas.tamasauskas.73/"
+          target="_blank"
+          rel="noopener"
+          title="Facebook"
+          ><img
+            alt="Irmantas Tamašauskas Facebook"
+            title="Facebook"
+            src="../assets/facebook.svg"
+          />irmantas.tamasauskas.73</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/irmantastamasauskas/"
+          target="_blank"
+          rel="noopener"
+          title="Instagram"
+          ><img
+            alt="Irmantas Tamašauskas Instagram"
+            title="Instagram"
+            src="../assets/instagram.svg"
+          />@irmantastamasauskas</a
+        >
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+li:not(:last-child) {
+  margin-bottom: 4px;
 }
+
 a {
-  color: #42b983;
+  color: var(--secondary-color);
+}
+
+a img {
+  width: 30px;
+  margin-right: 8px;
+  top: -1px;
+  position: relative;
+  vertical-align: middle;
 }
 </style>
