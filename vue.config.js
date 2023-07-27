090e9b78d0d14a4a159f7fd75a13ae47173ef978
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-07-27 16:14:22 +0300

    Add dummy meta description

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,8 @@ module.exports = {
       // entry for the page
       entry: "src/main.js",
       title: "Irmantas Tamašauskas",
+      description:
+        "My name is Irmantas and I'm a Front End developer counting 9 professional years in the field",
       url: "irmantastamasauskas.com",
     },
   },
