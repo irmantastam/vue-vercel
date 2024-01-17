93e92aff4de8ea17fa4a1b62da3beca918baaeec
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2024-01-17 18:46:18 +0200

    Add enableSystem on ThemeProvider

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -20,7 +20,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       enableLiveUpdates={pageProps.previewActive}
       locale={locale || 'en-US'}
     >
-      <ThemeProvider attribute="class">
+      <ThemeProvider attribute="class" enableSystem={true}>
         <>
           <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ''} />
           <main className={`${urbanist.variable} font-sans`}>
