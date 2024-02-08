82469d54f44bc8f5b6e9b4cd6258da7481d85b12
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2024-02-08 21:50:03 +0200

    Rename en locale to en-US

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'lt'],
+    defaultLocale: 'en-US',
+    locales: ['en-US', 'lt'],
     localeDetection: false,
   },
   localePath: path.resolve('./public/locales'),
